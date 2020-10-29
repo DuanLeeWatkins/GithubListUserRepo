23f3ec267f77f8b8c7c56ba5b24c3f43c8f9bc19
Duan Lee-Watkins <leeduan19@gmail.com>
2020-10-29 16:47:10 -0400

    updated the style.js and added roadmap.js

--- style.js
+++ style.js
@@ -1,48 +1,43 @@
+function displayResults(url, name) {
 
-//function displayResults(responseJson) {
-    //console.log(responseJson);
+    console.log(responseJson);
 
-    //let result =
-//}
+    $('#results-list').empty();
 
-//function formatQueryParam(param) {
-    //const queryItems = Object.keys(param)
-    //    .map(key => `${key}=${param[key]}`)
-    //return queryItems;
-//}
+    for (let i = 0; i < responseJson.value.length; i++) {
+        $('#results-list').append(`<li><h3>${responesJson.value[i].name}</h3>
+        <a href="${responseJson.value[i].url}"></a>
+        </li>`)
+    }
+
+    $('#results').removeClass('hidden');
+};     
 
 
-function getRepos(query) {
-    const searchURL = `https://api.github.com/users/DuanLeeWatkins/repos`
-    //const params = {
-        //q: "DuanLeeWatkins"
-    //};
 
-    //const queryString = formatQueryParam(param)
-    //const url = searchURL + queryString + "/repos";
 
-    //console.log(url);
+
+function getRepos(userHandle) {
+    const searchURL = `https://api.github.com/users/${userHandle}/repos`
+    
 
     fetch(searchURL)
         .then(response => {
-            if (response.ok) {
-                return response.json()
-            }
-                
+            if (response.ok) return response.json()
+            return response.json().then((e) => Promise.reject(e))
         })
-        .then(responseJson => console.log(responseJson))
+        .then(responseJson => displayResults(responseJson))
         .catch(error => {
             alert(error.message)
         });
-    console.log(responseJson)
 }
 
 
 function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
-        const searchUser = $('#user-handle').val();
-        getRepos(searchUser);
+        let input = $('#user-handle').val();
+        getRepos(input);
         
     });
 }
